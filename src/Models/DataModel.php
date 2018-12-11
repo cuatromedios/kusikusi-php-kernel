728c4c01349514181a374f5e54370f17db53491c
Ramses <ramses@cuatromedios.com>
2018-12-10 19:20:31 -0600

    Simplifying the relations, only Entity will hava data model relations and no viceversa. User model as example.

--- src/Models/DataModel.php
+++ src/Models/DataModel.php
@@ -15,56 +15,25 @@ class DataModel extends KusikusiModel
   public $modelId = 'nomodel';
   protected $table = DataModel::NO_DATA_TABLE;
   protected $_entity;
-  // protected $appends = ['entity', 'contents'];
-  // protected $with = ['relatedContents', 'relatedEntity'];
-  protected $hidden = ['relatedEntity', 'relatedContents'];
 
   public function __construct(array $newAttributes = array(), $lang = NULL)
   {
     parent::__construct($newAttributes, $lang);
-    $this->_entity = Entity::firstOrNew(["id" => $this->attributes['id']], ["model" => self::modelId()]);
   }
 
   /**
    * Set the relation to an Entity.
    */
-  public function relatedEntity() {
+  public function entity() {
     return $this->belongsTo('App\Models\Entity', 'id');
   }
 
-  /**
-   * Mutator to create or update the entity alongside the related model
-   * @param array $value
-   */
-  public function setEntityAttribute($values) {
-    $merged_values = array_merge(["id" => $this->attributes['id']], ["model" => self::modelId()], $values);
-    $this->_entity = Entity::firstOrNew(["id" => $this->attributes['id']], $merged_values);
-    $this->_entity->attributes = array_merge($this->_entity->attributes, $values);
-  }
-
-  /**
-   * Mutator to retrieve alongside the related model
-   * @return Entity Returns the related entity
-   */
-  public function getEntityAttribute() {
-    return $this->_entity;
-  }
-
   public static function boot() {
-
     parent::boot();
-
     self::creating(function ($model) {
       if ($model->table == DataModel::NO_DATA_TABLE) {
         $model->model = self::modelId();
       }
-    });
-    self::saved(function ($model) {
-        $model->_entity->clearContents();
-        $model->_entity->save();
-    });
-    self::retrieved(function ($model) {
-      $model->_entity = $model->relatedEntity;
     });
     static::addGlobalScope(new ModelScope);
   }
