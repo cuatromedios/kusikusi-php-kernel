f4abf7618a1ee897ddf3ea8c0670c9376ca9d44d
Ramses <ramses@cuatromedios.com>
2018-12-15 19:34:33 -0600

    The seeding of sample admin now includes the permission to root

--- src/Exceptions/ExceptionDetails.php
+++ src/Exceptions/ExceptionDetails.php
@@ -8,29 +8,34 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ExceptionDetails
 {
-    public static function filter(\Exception $e) {
-        $result = ["info" => []];
-        switch (get_class($e)) {
-            case "Illuminate\Database\QueryException":
-                $result["info"]["code"] = ApiResponse::STATUS_BADREQUEST;
-                $result["info"]["message"] = ApiResponse::TEXT_BADREQUEST;
-                break;
-            case "Illuminate\Database\Eloquent\ModelNotFoundException":
-                $result["info"]["code"] = ApiResponse::STATUS_NOTFOUND;
-                $result["info"]["message"] = ApiResponse::TEXT_NOTFOUND;
-                break;
-            default:
-                $result["info"]["code"] = ApiResponse::STATUS_INTERNALERROR;
-                $result["info"]["message"] = ApiResponse::TEXT_INTERNALERROR;
-        }
-        if ('local' === env('APP_ENV', config('app.env', 'production'))) {
-            $result['info']['exception'] = [
-                "error" => $e->getMessage(),
-                "file" => $e->getFile(),
-                "line" => $e->getLine(),
-                "trace" => $e->getTraceAsString()
-            ];
-        }
-        return $result;
+  public static function filter(\Exception $e)
+  {
+    $result = ["info" => []];
+    switch (get_class($e)) {
+      case "Illuminate\Database\QueryException":
+        $result["info"]["code"] = ApiResponse::STATUS_BADREQUEST;
+        $result["info"]["message"] = ApiResponse::TEXT_BADREQUEST;
+        break;
+      case "Illuminate\Database\Eloquent\ModelNotFoundException":
+        $result["info"]["code"] = ApiResponse::STATUS_NOTFOUND;
+        $result["info"]["message"] = ApiResponse::TEXT_NOTFOUND;
+        break;
+      case "Illuminate\Validation\ValidationException":
+        $result["info"]["code"] = ApiResponse::STATUS_BADREQUEST;
+        $result["info"]["message"] = $messages = implode(" ", array_flatten($e->validator->getMessageBag()->getMessages()));
+        break;
+      default:
+        $result["info"]["code"] = ApiResponse::STATUS_INTERNALERROR;
+        $result["info"]["message"] = ApiResponse::TEXT_INTERNALERROR;
     }
+    if ('local' === env('APP_ENV', config('app.env', 'production'))) {
+      $result['info']['exception'] = [
+          "error" => $e->getMessage(),
+          "file" => $e->getFile(),
+          "line" => $e->getLine(),
+          "trace" => $e->getTraceAsString()
+      ];
+    }
+    return $result;
+  }
 }
--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -3,95 +3,113 @@
 namespace Cuatromedios\Kusikusi\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class Permission extends Model
 {
 
-    const NONE = 'none';
-    const OWN = 'own';
-    const ANY = 'any';
+  const NONE = 'none';
+  const OWN = 'own';
+  const ANY = 'any';
 
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'permissions';
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'permissions';
 
-    protected $primaryKey = 'user_id';
-
-
-    /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
-
-    /**
-     * Indicates  the model should be timestamped.
-     *
-     * @var bool
-     */
-    public $timestamps = false;
-
-    /**
-     * Get the EntityBase that owns the content.
-     */
-    public function user()
-    {
-        return $this->belongsTo('App\Models\Data\User', 'id');
-    }
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'user_id', 'entity_id', 'read', 'write'
-    ];
+  protected $primaryKey = 'user_id';
 
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [];
+  /**
+   * Indicates if the IDs are auto-incrementing.
+   *
+   * @var bool
+   */
+  public $incrementing = false;
 
-    /**
-     * Get permissions.
-     *
-     * @param $id
-     * @return \Illuminate\Http\JsonResponse|array
-     */
-    public static function getPermissions($id) {
-        $user = Permission::where('user_id', $id)->firstOrFail();
-        $user->get();
-        return $user;
-    }
+  /**
+   * Indicates  the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = false;
 
-    /**
-     * Post permissions.
-     *
-     * @param $information
-     * @return \Illuminate\Http\JsonResponse|array
-     */
-    public static function postPermissions($information) {
-        $user = Permission::create($information);
-        return $user;
-    }
+  /**
+   * Get the EntityBase that owns the content.
+   */
+  public function user()
+  {
+    return $this->belongsTo('App\Models\User', 'id');
+  }
 
-    /**
-     * Patch permissions.
-     *
-     * @param $user_id, $information
-     * @return \Illuminate\Http\JsonResponse|array
-     */
-    public static function patchPermissions($user_id, $information) {
-        $user = Permission::where('user_id', $user_id)->firstOrFail();
-        $user->update($information);
-        return $user;
-    }
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'user_id', 'entity_id', 'read', 'write'
+  ];
+
+
+  /**
+   * The attributes excluded from the model's JSON form.
+   *
+   * @var array
+   */
+  protected $hidden = [];
+
+  /**
+   * Get permissions.
+   *
+   * @param $user_id
+   * @return Collection
+   */
+  public static function getPermissions($user_id)
+  {
+    $permissions = Permission::where('user_id', $user_id)->get();
+    return $permissions;
+  }
+
+  /**
+   * Post permissions.
+   *
+   * @param $information
+   * @return \Illuminate\Http\JsonResponse|array
+   */
+  public static function addPermission($user_id, $entity_id, $read, $write)
+  {
+    $vars = get_defined_vars();
+    Validator::make($vars, [
+        "user_id" => "required",
+        "entity_id" => "required",
+        "read" => "required",
+        "write" => "required|in:".Permission::NONE.",".Permission::OWN.",".Permission::ANY
+    ], Config::get('validator.messages'))->validate();
+    Permission::deletePermission($user_id, $entity_id);
+    $result = Permission::create([
+        "user_id" => $user_id,
+        "entity_id" => $entity_id,
+        "read" => $read,
+        "write" => $write
+    ]);
+    return $result;
+  }
+
+  /**
+   * Patch permissions.
+   *
+   * @param $user_id , $information
+   * @return \Illuminate\Http\JsonResponse|array
+   */
+  public static function deletePermission($user_id, $entity_id)
+  {
+    $result = Permission::where('user_id', $user_id)->where('entity_id', $entity_id)->delete();
+    return $result;
+  }
 }
