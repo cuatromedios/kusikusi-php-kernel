8e6c368230aa71291d26e58a67af08acda8d875a
alograg <alograg@gmail.com>
2019-04-02 21:02:57 +0200

    Configuration command

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace Cuatromedios\Kusikusi\Console;
 
+use Cuatromedios\Kusikusi\Console\Commands\PostInstall;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        PostInstall::class
     ];
 
     /**
